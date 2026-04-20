3f0370fc0a48c50861a52ac257ed0ef6475fa3ca
Edson Lima <ead33843576@gmail.com>
2026-04-20 17:06:26 -0300

    fix/ajustes

--- sales-flow-api/src/main.ts
+++ sales-flow-api/src/main.ts
@@ -1,14 +1,14 @@
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  
-  const app = await NestFactory.create(AppModule, { bodyParser: true });
-    app.enableCors({
-    origin: 'http://localhost:5173',
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
     credentials: true,
   });
-  await app.listen(process.env.DEFAULT_PORT || 3000);
+
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
